bb3f7dd96659e1400f62e2f0a759663c46cf6e75
mtuboi <mtuboi@sciencepark.co.jp>
2021-02-08 11:25:59 +0900

    マジックナンバーを定義値に変更

--- 001_将棋エンジンを作ってみる【ルール実装編1】Bitboard入門/source/bitboard.h
+++ 001_将棋エンジンを作ってみる【ルール実装編1】Bitboard入門/source/bitboard.h
@@ -4,11 +4,13 @@
 class Bitboard {
     public:
         Bitboard();
-        Bitboard(uint64_t d);
-        Bitboard(uint64_t u, uint64_t d);
+        Bitboard(uint64_t l);
+        Bitboard(uint64_t u, uint64_t l);
         uint64_t GetU() const;
-        uint64_t GetD() const;
+        uint64_t GetL() const;
 
     private:
+        const int LOWER = 0;
+        const int UPPER = 1;
         uint64_t bb[2];
 };
